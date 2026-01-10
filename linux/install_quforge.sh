a324573e4d88aa92884c5aaa695ece8fe77c9b35
Esai Vel Murugan <esaivelmurugans@gmail.com>
2026-01-10 15:12:46 +0530

    Add files via upload

---
+++ linux/install_quforge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the current directory path
+APP_PATH=$(pwd)
+
+# Create the desktop entry
+cat <<EOF > QuForge.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=QuForge Industrial Studio
+Exec=python3 $APP_PATH/gui.py
+Icon=$APP_PATH/icon.png
+Path=$APP_PATH/
+Terminal=false
+Categories=Development;Science;IDE;
+EOF
+
+# Move it to the system applications folder
+mkdir -p ~/.local/share/applications
+cp QuForge.desktop ~/.local/share/applications/
+chmod +x ~/.local/share/applications/QuForge.desktop
+
+echo "Installation complete! You can now find QuForge Studio in your application menu."
